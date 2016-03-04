ee1bda3d8db9cad886636968c696c1fd6f009ec9
Hemanth <manthu522@gmail.com>
2016-03-04 18:49:43 +0530

    validations for apis

--- controllers/contact.js
+++ controllers/contact.js
@@ -8,23 +8,25 @@ var Contact = require("../models/contact.js"),
  */
 this.createContact = function(req, res, next){
 
-  logger.info('Controller: executing createContact() ');
+  insertValidate(req, next, function() {
 
-  var contact = new Contact(req.body);
+    logger.info('Controller: executing createContact() ');
 
-  Contact.create(req.body, function (err, contact) {
-    if (err){
-      console.log("Error : ",err);
-      next(err);
-    }else{
-      res.send({
-          'status' : 'success',
-          'message' : 'Contact created successfully ',
-          'contact' : contact
-      });
-    }
+    var contact = new Contact(req.body);
+
+    Contact.create(req.body, function (err, contact) {
+      if (err){
+        console.log("Error : ",err);
+        next(err);
+      }else{
+        res.send({
+            'status' : 'success',
+            'message' : 'Contact created successfully ',
+            'contact' : contact
+        });
+      }
+    });
   });
-
 };
 
 this.updateContact = function(req, res, next){
@@ -32,19 +34,18 @@ this.updateContact = function(req, res, next){
   logger.info('Controller: executing updateContact() ');
 
   //var contact = new Contact(req.body);
-
-  Contact.findByIdAndUpdate(req.body.contact_id, req.body, function(err, contact) {
-    if (err){
-      console.log("Error : ",err);
-      next(err);
-    }else{
-      res.send({
-          'status' : 'success',
-          'message' : 'Contact updated successfully ',
-          'contact' : contact
-      });
-    }
-  });
+    Contact.findByIdAndUpdate(req.body.contact_id, req.body, function(err, contact) {
+      if (err){
+        console.log("Error : ",err);
+        next(err);
+      }else{
+        res.send({
+            'status' : 'success',
+            'message' : 'Contact updated successfully ',
+            'data' : contact
+        });
+      }
+    });
 };
 
 
@@ -72,4 +73,78 @@ this.listContacts = function(req, res, next){
 
     }
   });
+};
+
+
+var insertValidate = function(req, callbackError, callbackOK) {
+  var self = this;
+  var index = 0;
+  var callbacks = [];
+  var errors = {};
+  var response = {};
+  var obj = {};
+
+  if (typeof req.body.contact_id !== 'undefined') {
+    obj._id = req.body.contact_id;
+  }
+  // Check user_email
+  callbacks.push(function() {
+        obj.email = req.body.email;
+
+        logger.info("obj ::", obj)
+        Contact.find(obj, function(error, email_result){
+            if (error) {
+              callbackError(error);
+            }
+
+            if (typeof email_result[0] !== "undefined") {
+              errors.user_email = "Email '" + req.body.email + "' is used.";
+            }
+            callbacks[++index]();
+        });
+  });
+
+  // Check user_contact
+  callbacks.push(function() {
+    obj.home_phone = req.body.home_phone;
+    Contact.find(obj, function(error, contact_result){
+        if (error) {
+          callbackError(error);
+        }
+
+        if (typeof contact_result[0] !== "undefined") {
+          errors.user_contact = "Mobile '" + self.home_phone + "' is used.";
+        }
+        callbacks[++index]();
+    });
+  });
+
+  // Check user_contact
+  callbacks.push(function() {
+    obj.land_phone = req.body.land_phone;
+    Contact.find(obj, function(error, land_phone){
+        if (error) {
+          callbackError(error);
+        }
+
+        if (typeof land_phone[0] !== "undefined") {
+          errors.user_contact = "Mobile '" + self.land_phone + "' is used.";
+        }
+        callbacks[++index]();
+    });
+  });
+
+  // Check if have errors
+  callbacks.push(function() {
+    if (Object.keys(errors).length === 0) {
+      callbackOK();
+    } else {
+      response.status = "error";
+      response.message = "Some of the details were already in use.";
+      response.errors = errors;
+      callbackError(response);
+    }
+  });
+
+  callbacks[index]();
 };
--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function () {
 
+    window.data;
+
     (function ($) {
 
         $('#filter').keyup(function () {
@@ -21,7 +23,6 @@ $(document).ready(function () {
         	type : "GET",
         	url : "http://localhost:9999/contact/select",
         	success : function(resp){
-        		console.log(resp);
         		prepareData(resp);
         		
         	},
@@ -33,7 +34,6 @@ $(document).ready(function () {
     }(jQuery));
 
     var contactDetails = function(contact){
-    	console.log("JJJJ", contact)
     	$("#d_name").html('<b>'+contact.first_name+' '+contact.last_name);
     	detail_row = '<tr><td> <b>'+contact.first_name+' '+contact.last_name +'</b></td></tr>';
 		var details_li = '';
@@ -44,47 +44,64 @@ $(document).ready(function () {
 		details_li += '<li class="list-group-item"> <h4 class="list-group-item-heading">address</h4>'+contact.address+'</li>';    	
 
     	$("#details").empty().append(details_li);
-
+        window.data = contact;
 
     }
 
 	$("#new_contact").click(function(){
-		//$( "#test" ).load( "add_contact.html" );
-		$("#create_modal").modal('show');
+        $("#first_name").val('');
+        $("#last_name").val('');
+        $("#user_email").val('');
+        $("#land_phone").val('');
+        $("#home_phone").val('');
+        $("#address").val('');
+        //$( "#test" ).load( "add_contact.html" );
+        $("#create_modal").modal('show');
 
-		$("#db_save").click(function(){
+		/*$("#db_save").click(function(){
 			var data ={};
 			data.first_name = $("#first_name").val();
 			data.last_name = $("#last_name").val();
 			data.email = $("#user_email").val();
 			data.land_phone = $("#land_phone").val();
 			data.home_phone = $("#home_phone").val();
-			data.address = $("#address").val();
+            data.address = $("#address").val();
+            window.data = data;
 
-			saveContact(data);
+            saveContact(data);
 
-		});
+		});*/
 
 	});
 
 	var saveContact = function(data){
-		console.log(JSON.stringify(data));
+        var url ='',method;
+        if(!data.contact_id){
+            url = "http://localhost:9999/contact/create";
+            method = "POST";
+        }else{
+            url = "http://localhost:9999/contact/update";
+            method = "PUT";
+        }
+        console.log('method ',method)
 		$.ajax({
-        	type : "POST",
+        	type : method,
         	contentType: "application/json",
-        	url : "http://localhost:9999/contact/create",
+        	url : url,
         	data : JSON.stringify(data),
         	success : function(resp){
-        		console.log(resp)
-        		if(resp.status === 'success'){
-        			location.reload();
-        			$(".alert-success").text(resp.message);
-        			
-        		}else{
-        			$("#error_messages").text(resp.message);
-        		}
-        		//$("#create_modal").modal('close');
-        		//prepareData(resp);
+                console.log(resp);
+                if(resp.status === 'success'){
+                  $("#create_modal").modal('hide');
+                  //prepareData(resp);
+                    //location.reload();
+                    showMessages(resp);
+                    //hideErrors();
+                }else{
+                    hideErrors();
+                    showErrors(resp.errors);
+                    $("#error_messages").text(resp.message);
+                }
         	},
         	error : function(err){
         		console.log(err);
@@ -95,9 +112,10 @@ $(document).ready(function () {
 
 
 	var prepareData = function(resp){
+        console.log(resp)
 		var contact_row = '';
 		$.each(resp.data, function( index, contact ) {
-		  	contact_row += '<tr first_name="'+contact.first_name+'" last_name="'+contact.last_name+'" address="'+contact.address+'" land_phone="'+contact.land_phone+'" home_phone="'+contact.home_phone+'" email="'+contact.email+'"><td> <a class="contacts_list" href="javascript:void(0)">'+contact.first_name+' <b>'+contact.last_name +'</b></td></a></tr>';
+		  	contact_row += '<tr contact_id="'+contact._id+'" first_name="'+contact.first_name+'" last_name="'+contact.last_name+'" address="'+contact.address+'" land_phone="'+contact.land_phone+'" home_phone="'+contact.home_phone+'" email="'+contact.email+'"><td> <a class="contacts_list" href="javascript:void(0)">'+contact.first_name+' <b>'+contact.last_name +'</b></td></a></tr>';
 		});
 
 		$("#contacts").empty().append(contact_row);
@@ -111,8 +129,92 @@ $(document).ready(function () {
 	    	obj.land_phone = closest_tr.attr('land_phone');
 	    	obj.email = closest_tr.attr('email');
 	    	obj.address = closest_tr.attr('address');
+            obj.contact_id = closest_tr.attr('contact_id');
 	    	contactDetails(obj);
 	    });
 	};
 
+
+    $("#edit_link").click(function(e){
+        hideErrors();
+        if(window.data){
+            $("#first_name").val(window.data.first_name);
+            $("#last_name").val(window.data.last_name);
+            $("#user_email").val(window.data.email);
+            $("#land_phone").val(window.data.land_phone);
+            $("#home_phone").val(window.data.home_phone);
+            $("#address").val(window.data.address);
+
+            $("#create_modal").modal('show');
+
+            $("#db_save").click(function(){
+                var data ={};
+                data.first_name = $("#first_name").val();
+                data.last_name = $("#last_name").val();
+                data.email = $("#user_email").val();
+                data.land_phone = $("#land_phone").val();
+                data.home_phone = $("#home_phone").val();
+                data.address = $("#address").val();
+                data.contact_id = window.data.contact_id;
+
+                saveContact(data);
+            });
+        }else{
+            alert("Please select altest one contact to edit !!")
+        }
+
+    });
+
+    var showErrors = function(errors) {
+        $.each(errors, function(id, message) {
+            $("#" + id + "_danger").html(message);
+            $("#" + id + "_group").addClass("has-error");
+        });
+    };
+
+    var hideErrors = function() {
+        $(".text-danger").html("");
+        $(".form-group").removeClass("has-error");
+        $(".help-danger").html("");
+    };
+    
+    var showMessages = function(resp) {
+        
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        
+        $("#messages").removeClass().addClass("alert").empty();
+        if (!resp.message) {
+            $("#messages").hide();
+            return ;
+        }
+        var className = "";
+        if (resp.status == "success") {
+            className = "alert-success";
+        } else if(resp.status == "info") {
+            className = "alert-info";
+        }else {
+            className = "alert-danger";
+        }
+        $("#messages").addClass(className);
+        $("#messages").append("<div>" + resp.message + "</div>");
+        /*$.each(resp.messages, function(message) {
+        });*/
+        $("#messages").show();
+    
+    };
+
+    $("#db_save").click(function(){
+        var data ={};
+        data.first_name = $("#first_name").val();
+        data.last_name = $("#last_name").val();
+        data.email = $("#user_email").val();
+        data.land_phone = $("#land_phone").val();
+        data.home_phone = $("#home_phone").val();
+        data.address = $("#address").val();
+        
+        window.data = data;
+
+        saveContact(data);
+
+    });
 });
